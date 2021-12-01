edf91f217707eb95d0adee5e10e0f433c2da4e1f
victorismaelreeves <50830454+victorismaelreeves@users.noreply.github.com>
2021-12-01 02:20:15 +0100

    Bugs fixed and files re uploaded

---
+++ final/knn.py
@@ -0,0 +1,161 @@
+import numpy as np
+
+
+class KNN:
+    """
+    K-neariest-neighbor classifier using L1 ('manhattan') or L2 ('euclidean') loss
+    """
+
+    def __init__(self, k=1, metric=None):
+        self.k = k
+        self.metric = metric
+
+    def fit(self, X, y):
+        self.train_X = X
+        self.train_y = y
+
+    def predict(self, X, num_loops=0):
+        '''
+        Uses the KNN model to predict classes for the data samples provided
+
+        Arguments:
+        X, np array (num_samples, num_features) - samples to run
+           through the model
+        num_loops, int - which implementation to use
+        Returns:
+        predictions, np array of ints (num_samples) - predicted class
+           for each sample
+        '''
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        else:
+            dists = self.compute_distances_two_loops(X)
+
+        if self.train_y.dtype == np.bool:
+            return self.predict_labels_binary(dists)
+        else:
+            return self.predict_labels_multiclass(dists)
+
+    def compute_distances_two_loops(self, X):
+        '''
+        Computes distance from every sample of X to every training sample
+        Uses simplest implementation with 2 Python loops
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        '''
+        num_train = self.train_X.shape[0]
+        num_test = X.shape[0]
+        dists = np.zeros((num_test, num_train), np.float32)
+        if self.metric == 'manhattan':
+            for i_test in range(num_test):
+                for i_train in range(num_train):
+                    # TODO: Fill dists[i_test][i_train]
+                    dists[i_test][i_train] = np.sum(np.abs(self.train_X[i_train] - X[i_test]))
+        elif self.metric == 'euclidean':
+            for i_test in range(num_test):
+                for i_train in range(num_train):
+                    # TODO: Fill dists[i_test][i_train]
+                    dists[i_test][i_train] = np.sum((self.train_X[i_train] - X[i_test] ** 2))
+        return dists          
+
+    def compute_distances_one_loop(self, X):
+        '''
+        Computes distance from every sample of X to every training sample
+        Vectorizes some of the calculations, so only 1 loop is used
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        '''
+        num_train = self.train_X.shape[0]
+        num_test = X.shape[0]
+        dists = np.zeros((num_test, num_train), np.float32)
+        if self.metric == 'manhattan':
+            for i_test in range(num_test):
+                # TODO: Fill the whole row of dists[i_test]
+                # without additional loops or list comprehensions
+                dists[i_test, :] = np.sum(np.abs(self.train_X - X[i_test]), axis=1)
+        elif self.metric == 'euclidean':
+            for i_test in range(num_test):
+                # TODO: Fill the whole row of dists[i_test]
+                # without additional loops or list comprehensions
+                dists[i_test, :] = np.sum((self.train_X - X[i_test] ** 2), axis=1)
+        return dists
+
+    def compute_distances_no_loops(self, X):
+        '''
+        Computes L1 distance from every sample of X to every training sample
+        Fully vectorizes the calculations using numpy
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+
+        Returns:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        '''
+        num_train = self.train_X.shape[0]
+        num_test = X.shape[0]
+        # Using float32 to to save memory - the default is float64
+        dists = np.zeros((num_test, num_train), np.float32)
+        if self.metric == 'manhattan':
+            dists = np.abs(self.train_X - X[:, np.newaxis]).sum(-1)
+            
+        if self.metric == 'euclidean':
+            dists = np.sqrt(np.sum(X**2, axis=1, keepdims=True) - (2 * np.dot(X, self.train_X.T)) + np.sum(self.train_X**2, axis=1))
+        return dists    
+
+    def predict_labels_binary(self, dists):
+        '''
+        Returns model predictions for binary classification case
+
+        Arguments:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        Returns:
+        pred, np array of bool (num_test_samples) - binary predictions
+           for every test sample
+        '''
+        num_test = dists.shape[0]
+        pred = np.zeros(num_test, np.bool)
+        for i in range(num_test):
+            # TODO: Implement choosing best class based on k
+            # nearest training samples
+            ##closest = []
+            nearest = np.argsort(dists[i, :])[:self.k]
+            closest = self.train_y[nearest]
+            if self.k/2<np.sum(self.train_y[nearest[:self.k]]):
+                pred[i]=True
+            else:
+                pred[i]=False
+        return pred
+
+    def predict_labels_multiclass(self, dists):
+        '''
+        Returns model predictions for multi-class classification case
+
+        Arguments:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        Returns:
+        pred, np array of int (num_test_samples) - predicted class index
+           for every test sample
+        '''
+        num_test = dists.shape[0]
+        pred = np.zeros(num_test, np.int)
+        for i in range(num_test):
+
+            nearest = np.argsort(dists[i, :])[:self.k]
+            closest = self.train_y[nearest]
+            if self.k/2 < np.sum(self.train_y[nearest[:self.k]]):
+                pred[i]=True
+            else:
+                pred[i]=False
+        return pred
---
+++ final/metrics.py
@@ -0,0 +1,53 @@
+import numpy as np
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+    precision = 0
+    recall = 0
+    accuracy = 0
+    f1 = 0
+
+
+    accuracy = np.sum(prediction == ground_truth) / len(prediction)
+
+    
+
+    """
+    TP = np.sum(np.logical_and(pred_labels == 1, true_labels == 1))
+    TN = np.sum(np.logical_and(pred_labels == 0, true_labels == 0))
+    FP = np.sum(np.logical_and(pred_labels == 1, true_labels == 0))
+    FN = np.sum(np.logical_and(pred_labels == 0, true_labels == 1))
+    """
+
+    precision = np.sum(np.logical_and(prediction == ground_truth,prediction == True))/np.sum(prediction)
+    recall = np.sum(np.logical_and(prediction == ground_truth,prediction == True))/np.sum(ground_truth)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+    # https://towardsdatascience.com/accuracy-precision-recall-or-f1-331fb37c5cb9
+
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(prediction, ground_truth):
+    '''
+    Computes metrics for multiclass classification
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    '''
+    # TODO: Implement computing accuracy
+    print(prediction, ground_truth)
+    accuracy = sum(prediction == ground_truth) / len(prediction)
+    return accuracy
